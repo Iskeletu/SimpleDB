2fcde37e028e336d851de4aa1b7542524b444454
Iskeletu <iskeletu.br@gmail.com>
2021-12-03 16:59:32 -0300

    Fixed typo

--- Include/screens.h
+++ Include/screens.h
@@ -17,7 +17,7 @@ void PrintInvalidHelpScreen(string command);
 void PrintUnknownCommandScreen(string command);
 void PrintUnknownArgumentScreen(string command, string argument);
 void PrintInsufficientArgumentScreen(string command);
-void PrintUnknownArgumentScreen(string command, string argument, string expression);
+void PrintUnknownExpressionScreen(string command, string argument, string expression);
 void ClearScreen();
 //=============================
 
