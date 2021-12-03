d867eb8640f0ac8c2c70848396d75d73644e2149
Iskeletu <iskeletu.br@gmail.com>
2021-12-03 02:06:23 -0300

    Added namespace.

--- Include/commandhandler.h
+++ Include/commandhandler.h
@@ -1,5 +1,12 @@
+//Header definition
 #ifndef commandhandler_h_
 #define commandhandler_h_
+
+
+//==========Namespace==========
+using namespace std;
+//=============================
+
 
 //==========Functions==========
 bool ReadCommand(std::string user_input);
