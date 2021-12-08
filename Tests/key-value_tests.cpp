4bd094094379e01b6331ab4aee91bdd511e436c4
Iskeletu <iskeletu.br@gmail.com>
2021-12-08 04:13:23 -0300

    Implemented simple key-value tests

---
+++ Tests/key-value_tests.cpp
@@ -0,0 +1,31 @@
+//Libraries
+
+
+//Header file
+#include "catch.hpp"
+#include "dbhandler.h"
+
+
+//=====================Namespace====================
+using namespace std;
+//==================================================
+
+
+//==================Key-Value Test==================
+TEST_CASE("Store and retrive a value", "[setKeyValue,getKeyValue]")
+{ //Simple key-value store and retireve test
+    SECTION("Basic set and get")
+    {
+        string dbname("myemptydb");
+        Database db(Database::CreateDatabase(dbname));
+
+        // We know we have been successful when:-
+        // 1. The retrieved value is the same as the stored value
+        string key("string");
+        string value("defined_value");
+
+        db.InsertKeyValue(key, value);
+        REQUIRE(value == db.SearchKeyValue(key))
+    }
+}
+//==================================================
